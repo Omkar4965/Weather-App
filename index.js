b7760f07ccdad8024b23d6b76e62d8f1025f405b
Omkar4965 <omkarc4965@gmail.com>
2024-02-29 14:54:07 +0530

    updated

--- index.js
+++ index.js
@@ -1,48 +1,3 @@
-// // const API_KEY = "168771779c71f3d64106d8a88376808a";
-// // let para = document.querySelector(".newpara");
-// // async function callWhether(){
-// //     let city = "Maharashtra";
-
-// //     const response = await fetch(
-// //         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-// //       );
-
-// //     const data = await response.json();
-// //     console.log(data.main)
-// //     para.innerText = `Weather in ${city} is ${data.main.feels_like}°C`;
-// // }
-// // callWhether();
-
-// const url = "https://api.github.com/users/";
-// let user = document.querySelector(".userid");
-// let img = document.querySelector(".img");
-// let namee = document.querySelector(".namee");
-// let bio = document.querySelector(".bio");
-// let createdon = document.querySelector(".createdon");
-// let followers = document.querySelector(".followers");
-// let followings = document.querySelector(".following");
-
-// let data ;
-
-//   user.addEventListener('keypress',async () => {
-//     if (event.key === "Enter") {
-//       let userId = user.value;
-//       let userurl = url+userId;
-//       let response = await fetch(userurl);
-//        data =await response.json();
- 
-//        img.backgroundImage
-//        namee.innerHTML = data.name;
-//        img.backgroundImage = "url('data.avatar_url')"
-//        bio.innerHTML = data.bio
-//        createdon.innerHTML = data.
-//        followers.innerHTML = data.followers
-//        followings.innerHTML = data.followings
-//     }
-//   })
-
-
-
 const userTab = document.querySelector("[data-userweather]")
 const searchTab = document.querySelector("[data-searchweather]")
 const userContainer = document.querySelector(".weather-container")
@@ -145,6 +100,12 @@ grantAccessBtn.addEventListener('click', () => {
   }
 })
 
+searchBtn.addEventListener('click',() => {
+  let city = searchInput.value;
+  getSearchWeatherData(city);
+})
+
+
 // get location of search place
 searchInput.addEventListener('keypress',()=>{
 
@@ -170,6 +131,8 @@ function position(p){
     
     
 }
+
+
 
 // collecting weather data from lat and lon
 async function getWeatherData(lat, lon){
